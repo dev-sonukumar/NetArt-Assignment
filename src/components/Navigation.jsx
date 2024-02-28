67a6ac17d35f9b202a4b9a2f441df373175a118c
sonu kumar <dev.sonukumarsingh@gmail.com>
2024-02-28 17:24:22 +0530

    add image5

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,10 +3,7 @@ import React from "react";
 const Navigation = () => {
   return (
     <div className="container navContainer flex ">
-      <img
-        src="https://www.crifluidsystems.com/50/wp-content/uploads/2021/02/cri-logo-new.png"
-        alt="branding"
-      />
+      <img src="/public/images/logo.png" alt="branding" />
     </div>
   );
 };
